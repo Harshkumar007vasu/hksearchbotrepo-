7db06910196b3cecd6711cb2dac08f79a522ba48
Harshkumar007vasu <115921810+Harshkumar007vasu@users.noreply.github.com>
2022-12-25 13:23:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/@harsh00790/hksearchbotrepo.git /hksearchbotrepo
+git clone https://github.com/harsh00790/hksearchbotrepo.git /hksearchbotrepo
 cd /hksearchbotrepo
 pip3 install -r requirements.txt
 echo "Starting Bot...."
